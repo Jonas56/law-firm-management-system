d5272ee74ccc36ab0349f68ba4510b3bdba941f5
Jonas <youneslamkhanter02@gmail.com>
2021-12-11 13:53:57 +0100

    Health check route

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,5 +8,8 @@ const api = express.Router();
 api.use("/api/cases", middleware.userAuthentication, caseRouter);
 api.use("/api/users", userRouter);
 api.use("/api/login", loginRouter);
+api.get("/health", (req, res) => {
+  res.send("v0.1.0");
+});
 
 module.exports = api;
