41fc980fc5ad46246c3babd56585449df89bf6fb
Jonas <youneslamkhanter02@gmail.com>
2021-12-11 15:09:50 +0100

    Update .eslintrc.js

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "windows"],
+    "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     eqeqeq: "error",
